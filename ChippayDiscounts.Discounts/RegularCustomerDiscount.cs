c6aa43f699a3b7b326130b30f84f85b46a1605d8
Sylvan Bowdler <sylvanbowdler18@gmail.com>
2025-06-10 19:42:10 +0100

    add some guiding comments to RegularCustomerDiscount

--- ChippayDiscounts.Discounts/RegularCustomerDiscount.cs
+++ ChippayDiscounts.Discounts/RegularCustomerDiscount.cs
@@ -2,7 +2,15 @@ using ChippayDiscounts.Core;
 
 namespace ChippayDiscounts.Discounts;
 
+/// <summary>
+/// A discount for loyal Chippay customers.
+/// </summary>
 public class RegularCustomerDiscount : IDiscountStrategy
 {
+    /// <summary>
+    /// Get the discount to be applied to the user's basket.
+    /// </summary>
+    /// <param name="basket">The items the user has in their basket.</param>
+    /// <returns>The amount to be subtracted from the basket total.</returns>
     public decimal GetDiscount(IList<BasketItem> basket) => 0m;
 }
